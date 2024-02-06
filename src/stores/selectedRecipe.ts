97a56b2e03897554d01ab33b972fbc17ef3f4827
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-02-06 13:50:37 +0800

    Persisted Storage

--- src/stores/selectedRecipe.ts
+++ src/stores/selectedRecipe.ts
@@ -11,11 +11,12 @@ interface Recipe {
 export const useSelectedRecipeStore = defineStore({
   id: 'selectedRecipe',
   state: (): { item: Recipe | null } => ({
-    item: null,
+    item: JSON.parse(localStorage.getItem('selectedRecipe') || 'null'),
   }),
   actions: {
     selectItem(item: Recipe) {
       this.item = item;
+      localStorage.setItem('selectedRecipe', JSON.stringify(item));
     },
   },
 });
