f7b59fbd02336871beb0ecb0c799eab8c992b5f9
Kyle Anthony Nierras <20-1-01205@vsu.edu.ph>
2024-02-18 20:16:58 -0800

    Side bar index fix

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -142,7 +142,7 @@ onMounted(() => {
 
    <!-- Sidebar -->
    <aside id="logo-sidebar"
-      class="fixed top-0 left-0 z-30 w-64 h-screen pt-20 transition-transform -translate-x-full bg-slate-100 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+      class="fixed top-0 left-0 z-50 w-64 h-screen pt-20 transition-transform -translate-x-full bg-slate-100 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
       aria-label="Sidebar">
       <div class="h-full px-3 pb-4 overflow-y-auto bg-slate-100 dark:bg-gray-800 d-flex flex-column justify-content-between">
          <div class="flex flow-row justify-Start ml-2 items-center">
